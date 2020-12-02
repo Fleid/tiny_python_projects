172fef6790f932fe65c549e185e232d1dd53ff76
fleid <florian.eiden@fleid.fr>
2020-12-02 15:55:17 -0800

    Update syntax to split arg processing

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -5,11 +5,13 @@
 
 import argparse
 
-def main ():
+def get_args():
     parser = argparse.ArgumentParser(description='Say Hello')
     parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-    args = parser.parse_args()
+    return parser.parse_args()
 
+def main ():
+    args = get_args()
     print('Hello, ' + args.name + '!')
 
 if __name__ == '__main__':
