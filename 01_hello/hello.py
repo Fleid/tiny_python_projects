4588eeb6179c74f7a7469cf62e82fdaf58910250
fleid <florian.eiden@fleid.fr>
2020-12-02 16:01:49 -0800

    Lint with flake8

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
-# Gets python3 from env rather than current session
-
-# Purpose: Say Hello
+"""
+Author : Fleid
+Purpose : Say Hello
+"""
 
 import argparse
 
+
+# ------------------------------------------------------------------------
 def get_args():
+    """Get the command-line arguments"""
+
     parser = argparse.ArgumentParser(description='Say Hello')
-    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    parser.add_argument('-n', '--name',
+                        metavar='name', default='World', help='Name to greet')
     return parser.parse_args()
 
-def main ():
+
+# ------------------------------------------------------------------------
+def main():
+    """ Where things happen"""
+
     args = get_args()
     print('Hello, ' + args.name + '!')
 
+
+# ------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
